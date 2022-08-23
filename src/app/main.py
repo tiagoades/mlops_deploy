d83162f99f428b44b4c050bbf3e1254498060dba
tiagoades <tiago.almeida.s@gmail.com>
2022-08-23 11:38:28 -0300

    Alterado arquivo main.py

--- src/app/main.py
+++ src/app/main.py
@@ -1,25 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_basicauth import BasicAuth
 from textblob import TextBlob
-#import pandas as pd
-#from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 import os
 
+#Modelo de Regressão Linear serialidado em arquivo
 modelo = pickle.load(open('models/modelo.sav', 'rb'))
 
-#df = pd.read_csv('casas.csv')
 colunas  = ['tamanho', 'ano', 'garagem']
-#colunas  = ['tamanho', 'preco']
-#df = df[colunas]
-
-#x = df.drop('preco', axis=1) #remove a coluna preço para variaveis explicativas
-#y = df['preco'] #variavel resposta e o preço
-
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-#modelo = LinearRegression()
-#modelo.fit(x_train, y_train) #Ajusta o modelo para as 3 colunas
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
@@ -29,7 +18,7 @@ basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
-    return "is active!"
+    return "Trabalho de Conclusão de Curso!!!"
 
 @app.route('/sentimento/<frase>')
 @basic_auth.required
@@ -41,7 +30,6 @@ def sentimento(frase):
 
 @app.route('/cotacao/', methods=['POST'])
 @basic_auth.required
-
 def cotacao():
     dados = request.get_json()
     dados_input = [dados[col] for col in colunas]
